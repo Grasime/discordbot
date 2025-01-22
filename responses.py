90bc63d2ef2108c3c44f3484919d0b2b82d6b896
Grasime <davidstefan_16@yahoo.com>
2025-01-21 21:21:18 -0500

    initial comm

---
+++ responses.py
@@ -0,0 +1,22 @@
+from random import choice, randint
+
+def get_response(user_input: str) -> str:
+    lowered: str = user_input.lower()
+
+    if lowered == '':
+        return 'Well you don\'t speak much...'
+    elif 'hello' in lowered:
+        return 'Hello there!'
+    elif 'how are you' in lowered:
+        return 'Good, thanks!'
+    elif 'bye' in lowered:  # Fixed: Added missing colon
+        return 'See you!'
+    elif 'roll dice' in lowered:
+        return f'You rolled: {randint(1, 6)}'
+    else:
+        return choice([
+            'I do not understand what you are saying.',
+            'HUH?',
+            'WHAT DID YOU JUST SAY?'
+        ])
+
